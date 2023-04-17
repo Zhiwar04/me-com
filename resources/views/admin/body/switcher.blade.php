79e12f3f5e23a8f7cc5ebbf1bef536786f2b911a
Zhiwar04 <95308235+Zhiwar04@users.noreply.github.com>
2023-04-17 05:47:46 +0300

    added backend theme to the project and segmintation the pages

---
+++ resources/views/admin/body/switcher.blade.php
@@ -0,0 +1,113 @@
+<div class="switcher-wrapper">
+    <div class="switcher-btn"><i class="bx bx-cog bx-spin"></i></div>
+    <div class="switcher-body">
+        <div class="d-flex align-items-center">
+            <h5 class="mb-0 text-uppercase">Theme Customizer</h5>
+            <button type="button" class="btn-close ms-auto close-switcher" aria-label="Close"></button>
+        </div>
+        <hr />
+        <h6 class="mb-0">Theme Styles</h6>
+        <hr />
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="form-check">
+                <a id="lightmode">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-sun animated sun">
+                        <circle cx="12" cy="12" r="5"></circle>
+                        <line x1="12" y1="1" x2="12" y2="3"></line>
+                        <line x1="12" y1="21" x2="12" y2="23"></line>
+                        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+
+                        <line x1="1" y1="12" x2="3" y2="12"></line>
+                        <line x1="21" y1="12" x2="23" y2="12"></line>
+                        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                    </svg>
+                </a>
+            </div>
+            <div class="form-check">
+                <a id="darkmode">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-moon animated bounceIn moon">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="form-check">
+                <img src="../Backend Theme/assets/images/icons/half-sun.png" width="40" height="40"
+                    id="semidark" alt="" />
+            </div>
+        </div>
+        <hr />
+        <div class="form-check">
+            <input class="form-check-input" type="radio" id="minimaltheme" name="flexRadioDefault" />
+            <label class="form-check-label" for="minimaltheme">Minimal Theme</label>
+        </div>
+        <hr />
+        <h6 class="mb-0">Header Colors</h6>
+        <hr />
+        <div class="header-colors-indigators">
+            <div class="row row-cols-auto g-3">
+                <div class="col">
+                    <div class="indigator headercolor1" id="headercolor1"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor2" id="headercolor2"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor3" id="headercolor3"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor4" id="headercolor4"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor5" id="headercolor5"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor6" id="headercolor6"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor7" id="headercolor7"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator headercolor8" id="headercolor8"></div>
+                </div>
+            </div>
+        </div>
+
+        <hr />
+        <h6 class="mb-0">Sidebar Backgrounds</h6>
+        <hr />
+        <div class="header-colors-indigators">
+            <div class="row row-cols-auto g-3">
+                <div class="col">
+                    <div class="indigator sidebarcolor1" id="sidebarcolor1"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor2" id="sidebarcolor2"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor3" id="sidebarcolor3"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor4" id="sidebarcolor4"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor5" id="sidebarcolor5"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor6" id="sidebarcolor6"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor7" id="sidebarcolor7"></div>
+                </div>
+                <div class="col">
+                    <div class="indigator sidebarcolor8" id="sidebarcolor8"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
